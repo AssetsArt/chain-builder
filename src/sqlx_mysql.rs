23da42c0f122ff7367247f966d1c43bb68f125e1
Aitthi Arsa <meanstack20@gmail.com>
2024-02-16 10:38:28 +0700

    add count

--- src/sqlx_mysql.rs
+++ src/sqlx_mysql.rs
@@ -1,5 +1,6 @@
 use crate::ChainBuilder;
 use serde_json::Value;
+use sqlx::{self, Row};
 
 impl ChainBuilder {
     pub fn to_sqlx_query(
@@ -85,5 +86,51 @@ impl ChainBuilder {
             }
         }
         qb
+    }
+
+    pub async fn count(
+        &mut self,
+        column: &str,
+        pool: &sqlx::Pool<sqlx::MySql>,
+    ) -> Result<i64, sqlx::Error> {
+        let (_, binds) = self.to_sql();
+        let sql = self.sql_str.as_str();
+        let sql = format!("SELECT COUNT({}) FROM ({}) as count", column, sql);
+        let mut qb = sqlx::query(&sql);
+        for bind in binds {
+            match bind {
+                serde_json::Value::String(v) => {
+                    qb = qb.bind(v);
+                }
+                serde_json::Value::Number(v) => {
+                    if v.is_f64() {
+                        qb = qb.bind(v.as_f64().unwrap_or(0.0));
+                    } else if v.is_u64() {
+                        qb = qb.bind(v.as_u64().unwrap_or(0));
+                    } else if v.is_i64() {
+                        qb = qb.bind(v.as_i64().unwrap_or(0));
+                    } else {
+                        qb = qb.bind(v.to_string());
+                    }
+                }
+                serde_json::Value::Bool(v) => {
+                    qb = qb.bind(v);
+                }
+                serde_json::Value::Null => {
+                    let null_data: Option<Value> = None;
+                    qb = qb.bind(null_data);
+                }
+                serde_json::Value::Object(v) => {
+                    let to_string = serde_json::to_string(&v).unwrap_or_default();
+                    qb = qb.bind(to_string);
+                }
+                _ => {
+                    qb = qb.bind(bind);
+                }
+            }
+        }
+        let query_count = qb.fetch_one(pool).await?;
+        let count: i64 = query_count.try_get(0)?;
+        Ok(count)
     }
 }
